45365f45bbb37aedcb91ca30f8ed1f0f803ab5ab
mehdi_shz11 <mehdi_shz11@yahoo.com>
2018-11-19 09:52:19 +0330

    افزودن اینکو %20 به url encoder

--- videoview/src/main/java/rasa/com/videoview/Tools.java
+++ videoview/src/main/java/rasa/com/videoview/Tools.java
@@ -3,7 +3,7 @@ package rasa.com.videoview;
 import android.net.Uri;
 
 public class Tools {
-    private static final String ALLOWED_URI_CHARS = "@#&=*+-_.,:!?()/~'%";
+    private static final String ALLOWED_URI_CHARS = "@#&=*+-_.,:!?()/~'%20";
 
     public static String encodeUrl(String url){
         return Uri.encode(url, ALLOWED_URI_CHARS);
